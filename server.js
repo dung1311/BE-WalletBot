4290e5341c448ad5e715a7f5725648ab352d91d7
dung1311 <tiendung13112004@gmail.com>
2025-01-28 22:17:43 +0700

    Init structure of project

---
+++ server.js
@@ -0,0 +1,12 @@
+const app = require('./src/app');
+const process = require('process');
+
+const server = app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
+
+process.on('SIGINT', () => {
+    server.close(() => {
+        console.log('Server closed');
+    });
+});
