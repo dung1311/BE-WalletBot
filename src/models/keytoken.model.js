f72a2ae50eed4ab4a93e5b650da2af9252d70e70
dung1311 <109650915+dung1311@users.noreply.github.com>
2025-02-05 12:18:53 +0700

    [ADD]: add keytoken model

---
+++ src/models/keytoken.model.js
@@ -0,0 +1,34 @@
+"use strict";
+
+const { Schema, model, default: mongoose } = require("mongoose");
+
+const DOCUMENT_NAME = "Key";
+const COLLECTION_NAME = "Keys";
+
+const keyTokenSchema = new Schema(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    publicKey: {
+      type: String,
+      required: true,
+    },
+    privateKey: {
+      type: String,
+      required: true,
+    },
+    refreshToken: {
+      type: Array,
+      default: [],
+    },
+  },
+  {
+    collection: COLLECTION_NAME,
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model(DOCUMENT_NAME, keyTokenSchema);
