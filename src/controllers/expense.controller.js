718d279e44d036294c88a764ea150b987e8fac80
dung1311 <tiendung13112004@gmail.com>
2025-02-16 02:08:26 +0700

    feature/ finish CRUD for expense

--- src/controllers/expense.controller.js
+++ src/controllers/expense.controller.js
@@ -6,13 +6,33 @@ const FeeService = require("../services/expense.service");
 class FeeController {
     addExpense = asyncHandler(async (req, res, next) => {
         const response = await FeeService.addExpense(req.body);
-        return res.status(201).json(response);
+        return res.status(response.code).json(response);
     });
 
-    getFee = asyncHandler(async (req, res, next) => {
-        const response = await FeeService.getFee();
-        return res.status(200).json(response);
+    getExpense = asyncHandler(async (req, res, next) => {
+        const response = await FeeService.getExpense({});
+        return res.status(response.code).json(response);
     });
+
+    findExpense = asyncHandler(async (req, res, next) => {
+        const response = await FeeService.findExpense(req.params)
+        return res.status(response.code).json(response)
+    })
+
+    searchExpense = asyncHandler(async (req, res, next) => {
+        const response = await FeeService.searchExpense(req.params)
+        return res.status(response.code).json(response)
+    })
+
+    deleteExpense = asyncHandler(async (req, res, next) => {
+        const response = await FeeService.deleteExpenseById(req.params);
+        return res.status(response.code).json(response)
+    })
+
+    updateExpense = asyncHandler(async (req, res, next) => {
+        const response = await FeeService.updateExpense(req.params, req.body);
+        return res.status(response.code).json(response)
+    })
 }
 
 module.exports = new FeeController();
