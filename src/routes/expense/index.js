4daf6216a5d6f86f4397bd5814e964dae27f401e
nguyenhoangviethung <nguyenhoangviethung@gmail.com>
2025-02-24 16:30:34 +0700

    Update index.js

--- src/routes/expense/index.js
+++ src/routes/expense/index.js
@@ -8,7 +8,11 @@ const {authenticate, authenticateV2} = require("../../middleware/authMiddleware"
 
 // midleware
 router.use(authenticateV2)
-
+router.get("/getExpenseByAmount", feeController.getExpenseByAmount);
+router.get("/getExpenseByDate", feeController.getExpenseByDate);
+router.get("/getExpenseByCategory", feeController.getExpenseByCategory);
+router.get("/sortExpenses", feeController.sortExpenses);
+router.get("/sortPartner", feeController.sortPartner);
 router.get("/get-expense", feeController.getExpense);
 router.get("/:expenseId", feeController.findExpense)
 router.get("/search/:keySearch", feeController.searchExpense)
@@ -19,10 +23,6 @@ router.delete("/:expenseId", feeController.deleteExpense)
 
 router.patch("/:expenseId", feeController.updateExpense)
 
-router.get("/getExpenseByAmount", feeController.getExpenseByAmount);
-router.get("/getExpenseByDate", feeController.getExpenseByDate);
-router.get("/getExpenseByCategory", feeController.getExpenseByCategory);
-router.get("/sortExpenses", feeController.sortExpenses);
-router.get("/sortPartner", feeController.sortPartner);
+
 module.exports = router;
 
